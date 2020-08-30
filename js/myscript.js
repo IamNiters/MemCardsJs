778ba3d591f41e98906f165c7d64d79a58cc07cc
Niters <niters69@gmail.com>
2020-08-30 17:23:08 +0200

    Added Css Flexbox

--- js/myscript.js
+++ js/myscript.js
@@ -1,12 +1,1 @@
-function catchData(event) {
-  event.preventDefault();
-
-  let name = document.querySelector("#name").value;
-  let date = document.querySelector("#date").value;
-  let age = 2020 - date;
-
-  console.log("Hi, " + name + ", you are " + age + " years old.");
-}
-
-let myForm = document.querySelector("#MyHeroForm");
-myForm.addEventListener("submit", catchData);
+["🦄", "🐹", "🐼", "🐞", "🐷", "🐱", "🦊", "🐵"];
