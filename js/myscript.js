b55c182d905a24e7641750ab83972f308e0fc040
Niters <niters69@gmail.com>
2020-08-30 23:24:15 +0200

    template 2

--- js/myscript.js
+++ js/myscript.js
@@ -1,1 +1,19 @@
-let cards = ["🦄", "🐹", "🐼", "🐞", "🐷", "🐱", "🦊", "🐵"];
+let paquetCarte = ["🦄", "🐹", "🐼", "🐞", "🐷", "🐱", "🦊", "🐵"];
+
+function melangeur() {
+  let heroGameSection = document.querySelector("#HeroGameSection");
+  heroGameSection.innerHTML = "";
+
+  paquetCarte.forEach(function (element) {
+    let carte = document.createElement("div");
+    carte.innerHTML =
+      "<div class='HeroCard'>" +
+      "<div class='HeroCardContent'>" +
+      element +
+      "</div>";
+    ("</div>");
+    heroGameSection.appendChild(carte);
+  });
+}
+
+melangeur();
